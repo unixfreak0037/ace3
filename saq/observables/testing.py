965223afd64f4e34b1614e0a8bab388425f7d937
John Davison <unixfreak0037@gmail.com>
2025-07-27 16:45:41 -0400

    removed some commented out code

--- saq/observables/testing.py
+++ saq/observables/testing.py
@@ -1,5 +1,3 @@
-import base64
-import pickle
 from saq.analysis.observable import Observable
 from saq.constants import F_TEST
 from saq.observables.generator import map_observable_type
@@ -9,15 +7,5 @@ class TestObservable(Observable):
     __test__ = False # tell pytest this is not a test class
     def __init__(self, *args, **kwargs): 
         super().__init__(F_TEST, *args, **kwargs)
-
-    # this allows us to use any object we want for the observable value
-    # useful for passing around parameters for testing
-    #@property
-    #def value(self):
-        #return pickle.loads(base64.b64decode(self._value)) # TODO get rid of pickle
-
-    #@value.setter
-    #def value(self, v):
-        #self._value = base64.b64encode(pickle.dumps(v))
 
 map_observable_type(F_TEST, TestObservable)
